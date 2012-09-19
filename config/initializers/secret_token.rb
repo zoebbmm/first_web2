2b9d326b941dd5c464bf2b8a46fa7a500481d63c
zhou <zhou.wei@sv.cmu.edu>
2012-09-19 16:27:48 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstWeb2::Application.config.secret_token = '87962b1f6e4699e5b38a55689e687f5e70cc16fff4336c3817a8d8974e798e49c70973bf482abf74e9a2b59f8272e89ed6193dac9472b8d12f31950e59c4c9f3'
